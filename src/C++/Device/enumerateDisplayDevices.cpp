921dad68b55a12cc5a5446b9dc1d68790b23b436
JustXa0 <officialzappy15@gmail.com>
2023-08-10 04:08:36 -0500

    building better handling for gpu detection

--- src/C++/Device/enumerateDisplayDevices.cpp
+++ src/C++/Device/enumerateDisplayDevices.cpp
@@ -1,62 +1,23 @@
 #include "enumerateDisplayDevices.h"
 
 enumerateDisplayDevices::enumerateDisplayDevices()
-{  
-    Logger::getInstance().log_i("CREATING ENUMERATE DEVICES CLASS");
+{
+    // TODO: rewrite for error handling and multiple devices.
+    // I can't stand DirectX
 
-    DISPLAY_DEVICE displayDevice;
-    Devices devices;
-    devices.cb = sizeof(DISPLAY_DEVICE);
-    displayDevice.cb = sizeof(DISPLAY_DEVICE);
-    DWORD deviceIndex = 0;
+    Logger::getInstance().log_e("CREATING ENUMERATION CLASS");
 
-    while (EnumDisplayDevices(NULL, deviceIndex++, &displayDevice, 0))
-    {
-        DEVMODE devMode;
-        devMode.dmSize = sizeof(DEVMODE);
+    IDXGIFactory* pDXGIFactory = nullptr;
+    HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&pDXGIFactory);
 
-
-        //Not sure if needed, will be checked later        
-        if (EnumDisplaySettings(displayDevice.DeviceName, ENUM_CURRENT_SETTINGS, &devMode))
-        {
-
-        }
-
-        IDXGIFactory* pDXGIFactory = nullptr;
-        if (SUCCEEDED(CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&pDXGIFactory)))
-        {
-            IDXGIAdapter* pDXGIAdapter = nullptr;
-            if (SUCCEEDED(pDXGIFactory->EnumAdapters(deviceIndex - 1, &pDXGIAdapter))) {
-                DXGI_ADAPTER_DESC adapterDesc;
-                if (SUCCEEDED(pDXGIAdapter->GetDesc(&adapterDesc))) {
-                    std::wcout << "Adapter Description: " << adapterDesc.Description << std::endl;
-                    std::wcout << "Dedicated Video Memory: " << adapterDesc.DedicatedVideoMemory / (1024 * 1024) << " MB" << std::endl;
-                Logger::getInstance().log_i(adapterDesc.VendorId);
-                Logger::getInstance().log_i(adapterDesc.DeviceId);
-                //Logger::getInstance().log_i(adapterDesc.Description);
-                }
-                else
-                {
-                    // TODO: handle error here and log at highest level
-                    Logger::getInstance().log_c("FAILED TO FIND DESCRIPTION OF ADAPTER");
-                }
-                pDXGIAdapter->Release();
-            }
-            else
-            {
-                // TODO: handle error here and log at highest level
-                Logger::getInstance().log_c("FAILED TO ENUMERATE ADAPTERS");
-            }
-            pDXGIFactory->Release();
-        }
-        else
-        {
-            // TODO: handle error here and log at highest level
-            Logger::getInstance().log_c("FAILED TO CREATE FACTORY");
-        }
-
-    }
+    IDXGIAdapter* pDXGIAdapter = nullptr;
+    pDXGIFactory->EnumAdapters(0, &pDXGIAdapter);
     
+    DXGI_ADAPTER_DESC adapterDesc;
+    pDXGIAdapter->GetDesc(&adapterDesc);
+    
+    Logger::getInstance().log_e(adapterDesc.VendorId);
+
 }
 
 enumerateDisplayDevices::~enumerateDisplayDevices()
--- src/C++/Log/log.cpp
+++ src/C++/Log/log.cpp
@@ -9,7 +9,7 @@ Logger& Logger::getInstance()
 Logger::Logger() 
 {
     // TODO: fix so that path isn't hard-coded
-    logger = spdlog::basic_logger_mt("my_logger", "C:\\Users\\Jamie\\AppData\\Roaming\\rd-alps\\log.txt", true);
+    logger = spdlog::basic_logger_mt("my_logger", "%appdata%/rd-alps/log2.txt", true);
     spdlog::set_default_logger(logger);
     spdlog::set_level(spdlog::level::debug);
 }
